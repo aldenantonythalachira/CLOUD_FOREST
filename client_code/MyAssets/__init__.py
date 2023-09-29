d9de4bdbb9b97279f573308aab31e0f740056213
alden.thalachira@gmail.com <alden.thalachira@gmail.com>
2023-09-29 16:21:15 +0000

    Edited MyAssets

--- client_code/MyAssets/__init__.py
+++ client_code/MyAssets/__init__.py
@@ -14,7 +14,7 @@ class MyAssets(MyAssetsTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.load_asset()
+    self.load_assets()
     
   def render_asset(self, asset_id):
     self.content_panel.clear()
@@ -28,7 +28,7 @@ class MyAssets(MyAssetsTemplate):
     asset_panel = GridPanel()
     
     for i, asset in enumerate(assets):
-      c = AssetItem(farmer_name=asset["farmer_name"], location=asset["location"], image=asset["image"],cptpm=asset["cptpm"], trees_available=asset["trees_available"],rental_duration=asset["rental_duration"],total=asset["total"], button_click=self.render_asset)
+      c = AssetItem(asset_id=asset["id"],farmer_name=asset["farmer_name"], location=asset["location"], image=asset["image"],cptpm=asset["cptpm"], trees_available=asset["trees_available"],rental_duration=asset["rental_duration"],total=asset["total"], button_callback=False)
       asset_panel.add_component(c, row=str(i//2), width_xs=6)
     
     self.content_panel.add_component(asset_panel)
